8b4271181942c09cecb64144254ce14ee7c24310
ARUL ANAND S <arulanand.s@gmail.com>
2021-09-19 21:17:41 +0530

    add unit test case to verify sorting algorithm

---
+++ Tests/BubbleSortTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Service;
+
+namespace Tests
+{
+    [TestClass]
+    public class BubbleSortTests
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+
+        }
+
+        [TestMethod]
+        public void GivenUnsortedIntegers_WhenSortingCalled_ThenTheIntegerArraysShouldBeInSortedOrder()
+        {
+            var bubbleSortAlgorithm = new BubbleSortAlgorithm();
+            int[] unsortedIntegers = new int[] { 10, 49, 13, 7, 24, 78, 99, 56, 54, 31, 19, 3 };
+
+            var sortedIntegers = bubbleSortAlgorithm.DoSort(unsortedIntegers).GetAwaiter().GetResult();
+
+            var expectedOutput = new int[] { 3, 7, 10, 13, 19, 24, 31, 49, 54, 56, 78, 99 };
+
+            sortedIntegers.Should().BeEquivalentTo(expectedOutput);
+        }
+    }
+}
